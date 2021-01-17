97b0cf51ae4c12c66a1bbf130643a6f685bb874e
kryptonite43 <sonyj0430@gmail.com>
2021-01-18 01:46:14 +0900

    -recyclerview 기본설정 -catlist 생성 -strings '->""

--- app/src/main/java/com/example/cattest/MainActivity.kt
+++ app/src/main/java/com/example/cattest/MainActivity.kt
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    var catList = ArrayList<Cat>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.fragment_view_all)
+
     }
 }
---
+++ app/src/main/java/com/example/cattest/catData.kt
@@ -0,0 +1,3 @@
+package com.example.cattest
+
+data class Cat(var thumbnail: String?, var catname: String?, var desc1: String?,var desc2: String?,var desc3: String?,var desc4: String?)
