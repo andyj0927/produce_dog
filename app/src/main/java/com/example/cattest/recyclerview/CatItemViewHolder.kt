ceb8c8eb66b352868f7e37150441b254c658bf35
kryptonite43 <sonyj0430@gmail.com>
2021-01-18 19:57:18 +0900

    데이터 입력받기 진행중

--- app/src/main/java/com/example/cattest/recyclerview/CatItemViewHolder.kt
+++ app/src/main/java/com/example/cattest/recyclerview/CatItemViewHolder.kt
@@ -2,6 +2,15 @@ package com.example.cattest.recyclerview
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.example.cattest.Cat
+import kotlinx.android.synthetic.main.layout_cat_item.view.*
 
 class CatItemViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+    private val catImageView = itemView.cat_image
+    private val catName = itemView.cat_name
+    private val catInfo = itemView.cat_info
+    fun bindWithView(photoItem: Cat){
+        catName.text = photoItem.catname
+        catInfo.text = photoItem.desc
+    }
 }
