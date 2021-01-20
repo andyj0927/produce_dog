a599138139235e609a99c82e6cba6813feda95b3
dptmej0308 <68693148+dptmej0308@users.noreply.github.com>
2021-01-21 00:18:34 +0900

    버튼 클릭 별 기능 구현 1차
    추후 작동 확인 필요

--- app/src/main/java/fragment/QuestionFragment8.kt
+++ app/src/main/java/fragment/QuestionFragment8.kt
@@ -7,54 +7,54 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.cattest.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [QuestionFragment8.newInstance] factory method to
- * create an instance of this fragment.
- */
 class QuestionFragment8 : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    var tmp = algorithm(0,0,0,0)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    lateinit var navController: NavController
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_question8, container, false)
+        tmp.ei = arguments?.getInt("Data1")?:-1
+        tmp.sn = arguments?.getInt("Data2")?:-1
+        tmp.tf = arguments?.getInt("Data3")?:-1
+        tmp.jp = arguments?.getInt("Data4")?:-1
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment QuestionFragment8.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            QuestionFragment8().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?){
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+
+        btn_next.setOnClickListener(this)
+        option8_1.setOnClickListener(this)
+        option8_2.setOnClickListener(this)
+        btn_back.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.btn_next -> {
+                navController.navigate(R.id.action_questionFragment8_to_questionFragment9)
             }
+            R.id.option8_1 -> {
+                tmp.incrementTf()
+                navigationWithData(tmp.getEi(), tmp.getSn(), tmp.getTf(), tmp.getJp())
+            }
+            R.id.option8_2 -> {
+                navigationWithData(tmp.getEi(), tmp.getSn(), tmp.getTf(), tmp.getJp())
+            }
+            R.id.btn_back -> {
+                navController.popBackStack()
+            }
+        }
+    }
+
+    fun navigationWithData(data1: Int, data2: Int, data3: Int, data4: Int){
+        val bundle: Bundle = bundleOf("Data1" to data1,"Data2" to data2,"Data3" to data3,"Data4" to data4)
+        navController.navigate(R.id.action_questionFragment8_to_questionFragment9, bundle)
     }
 }
