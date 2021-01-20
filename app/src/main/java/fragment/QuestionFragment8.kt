5c1b24502349e8c300668fc8aae682b878daa96c
dptmej0308 <68693148+dptmej0308@users.noreply.github.com>
2021-01-21 00:26:46 +0900

    Update QuestionFragment8.kt
    viewOnclickListener부분 추가

--- app/src/main/java/fragment/QuestionFragment8.kt
+++ app/src/main/java/fragment/QuestionFragment8.kt
@@ -7,7 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.cattest.R
 
-class QuestionFragment8 : Fragment() {
+class QuestionFragment8 : Fragment(), View.OnClickListener {
     var tmp = algorithm(0,0,0,0)
 
     lateinit var navController: NavController
