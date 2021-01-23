1a0745b1795c3dbe669307ebdba61d026f1ebca0
kryptonite43 <sonyj0430@gmail.com>
2021-01-23 20:57:29 +0900

    adapter

--- app/src/main/java/com/example/cattest/recyclerview/CatGridRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/cattest/recyclerview/CatGridRecyclerViewAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.cattest.recyclerview
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -7,27 +8,20 @@ import com.example.cattest.App
 import com.example.cattest.Cat
 import com.example.cattest.R
 
-class CatGridRecyclerViewAdapter : RecyclerView.Adapter<CatItemViewHolder>(){
-    private var catList = ArrayList<Cat>()
+class CatGridRecyclerViewAdapter(private val context: Context) : RecyclerView.Adapter<CatItemViewHolder>(){
+    var data = mutableListOf<Cat>()
 
-    // 뷰홀더와 레이아웃 연결
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CatItemViewHolder {
-        val catItemViewHolder = CatItemViewHolder(LayoutInflater
-                                        .from(parent.context)
-                                        .inflate(R.layout.layout_cat_item, parent, false))
-        return catItemViewHolder
+        val view = LayoutInflater.from(context).inflate(R.layout.layout_cat_item,
+            parent, false)
+
+        return CatItemViewHolder(view)
     }
 
-    // 보여줄 목록의 개수
-    override fun getItemCount(): Int {
-        return this.catList.size
-    }
-    // 뷰가 묶였을때 데이터를 뷰홀더에 넘겨준다.
+    override fun getItemCount(): Int = data.size
+
     override fun onBindViewHolder(holder: CatItemViewHolder, position: Int) {
-        holder.bindWithView(this.catList[position])
-    }
-    // 외부에서 어답터에 데이터 배열을 넣어준다.
-    fun submitList(catList: ArrayList<Cat>){
-        this.catList = catList
+        holder.bindWithView(data[position])
     }
 }
+
