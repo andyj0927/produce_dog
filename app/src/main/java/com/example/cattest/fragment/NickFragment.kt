1ef83d1d7e5f2aded16b84b201e6e1c5f94d1716
dptmej0308 <68693148+dptmej0308@users.noreply.github.com>
2021-01-25 23:34:51 +0900

    QuestionFragment12까지의 답변을 통해 도출된 유형 String값을 nickFragment로 전달하는 부분 임시 코딩(주석처리 함)

--- app/src/main/java/com/example/cattest/fragment/NickFragment.kt
+++ app/src/main/java/com/example/cattest/fragment/NickFragment.kt
@@ -5,12 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.example.cattest.R
 import kotlinx.android.synthetic.main.fragment_nick.*
 
 class NickFragment : Fragment() {
+//    private var data = "cat"
     lateinit var navController: NavController
 
     override fun onCreateView(
@@ -18,7 +20,10 @@ class NickFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        //주석처리한 부분은 QuestionFragment12에서 결과값을 받아오기 위한 부분을 임시로 코딩한 것입니다.
+        // 유저에게 입력받을 닉네임 데이터와 함께 결과 값을 resultFragment 에서 쓰려면 어떻게 해야할 지 아직 몰라서 임시로 주석처리 해두었습니다.
+  //      data = arguments?.getString("Data")?:"cat"
+        return inflater.inflate(R.layout.fragment_nick, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -26,7 +31,9 @@ class NickFragment : Fragment() {
 
         navController = Navigation.findNavController(view)
         btn_next.setOnClickListener {
+    //        val bundle: Bundle = bundleOf("Data" to data)
             navController.navigate(R.id.action_nickFragment2_to_resultFragment2)
+    //      navController.navigate(R.id.action_nickFragment2_to_resultFragment2, bundle)
         }
     }
 
