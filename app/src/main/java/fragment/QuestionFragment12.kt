6f2960f8e600cdd683abb715b84acedbbdda4bd0
yoonseoJANG0809 <yoonseoj2@naver.com>
2021-01-25 22:34:06 +0900

    //question10 import수정

--- app/src/main/java/fragment/QuestionFragment12.kt
+++ app/src/main/java/fragment/QuestionFragment12.kt
@@ -9,7 +9,7 @@ import androidx.core.os.bundleOf
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.example.cattest.R
-import com.example.cattest.fragment.Algorithm
+import com.junga.cattest.fragment.Algorithm
 import kotlinx.android.synthetic.main.fragment_question12.*
 
 
@@ -44,7 +44,7 @@ class QuestionFragment12 : Fragment(), View.OnClickListener {
     override fun onClick(v: View?) {
         when(v?.id){
             R.id.btn_next -> {
-                navController.navigate(R.id.action_questionFragment12_to_nickFragment)
+                navController.navigate(R.id.action_questionFragment12_to_nickFragment2)
             }
             R.id.option12_1 -> {
                 tmp.incrementSn()
@@ -61,6 +61,6 @@ class QuestionFragment12 : Fragment(), View.OnClickListener {
 
     fun navigationWithData(data: String){
         val bundle: Bundle = bundleOf("Data" to data)
-        navController.navigate(R.id.action_questionFragment12_to_nickFragment, bundle)
+        navController.navigate(R.id.action_questionFragment12_to_nickFragment2, bundle)
     }
 }
