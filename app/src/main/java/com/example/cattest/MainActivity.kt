7ac13baa9f42c75f792a8b1b549451d71fa5bb77
aaho01176 <aaho01176@naver.com>
2021-02-13 21:00:03 +0900

    Login

--- app/src/main/java/com/example/cattest/MainActivity.kt
+++ app/src/main/java/com/example/cattest/MainActivity.kt
@@ -5,31 +5,8 @@ import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import kotlinx.android.synthetic.main.activity_main.*
-/*import fragment.NickFragment
 import fragment.ResultFragment
-
-class MainActivity : AppCompatActivity(), Communicator {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val fragment_nick = NickFragment()
-        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment_nick).commit()
-    }
-
-    override fun passDataCom(editTextInput: String){
-        val bundle = Bundle()
-        bundle.putString("message", editTextInput)
-
-        val transaction = this.supportFragmentManager.beginTransaction()
-        val fragment_result = ResultFragment()
-        fragment_result.arguments = bundle
-        transaction.replace(R.id.fragment_container, fragment_result)
-        transaction.commit()
-    }
-}
-*/
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -42,4 +19,5 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
     }
+
 }
--- app/src/main/java/fragment/NickFragment.kt
+++ app/src/main/java/fragment/NickFragment.kt
@@ -1,1 +1,31 @@
 package fragment
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.example.cattest.R
+
+class NickFragment : Fragment(){
+
+    lateinit var navController: NavController
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_nick, container, false)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+
+    }
+}
