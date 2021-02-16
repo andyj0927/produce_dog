d8d021124d2c3926274ca68685a1cb4cd7b6653a
dptmej0308 <68693148+dptmej0308@users.noreply.github.com>
2021-02-16 21:48:46 +0900

    question12->nick fragment -> result fragment 데이터 번들 전달 구현

--- app/src/main/java/fragment/NickFragment.kt
+++ app/src/main/java/fragment/NickFragment.kt
@@ -5,12 +5,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.example.cattest.R
+import kotlinx.android.synthetic.main.fragment_nick.*
 
 class NickFragment : Fragment(){
+    var option = "cat"
 
     lateinit var navController: NavController
 
@@ -19,13 +22,23 @@ class NickFragment : Fragment(){
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_nick, container, false)
+        option = arguments?.getString("Data")?:"cat"
 
+        return inflater.inflate(R.layout.fragment_nick, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         navController = Navigation.findNavController(view)
 
+        sendBtn.setOnClickListener{
+            navigationWithData(option)
+        }
+
+    }
+
+    fun navigationWithData(data: String){
+        val bundle: Bundle = bundleOf("Data" to data)
+        navController.navigate(R.id.action_nickFragment_to_resultFragment22, bundle)
     }
 }
